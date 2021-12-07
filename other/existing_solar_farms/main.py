ba365d82cd54cfe54c78f3f9a67224e6461b661b
twenty5151 <tsuiyikching@protonmail.com>
2021-12-07 20:35:18 +0000

    Export existing solar farms data to csv

--- other/existing_solar_farms/main.py
+++ other/existing_solar_farms/main.py
@@ -12,6 +12,11 @@ gdf_clipped = gdf_clipped.to_crs(26950)  # NAD83 / Arizona West
 gdf_clipped['centroid'] = gdf_clipped.centroid
 gdf_clipped.to_crs(az.crs)
 gdf_clipped['centroid'] = gdf_clipped['centroid'].to_crs(az.crs)
+# it can't export a geometry type for some reason...
+#gdf_clipped.to_file('out.shp')
+gdf_clipped['centroid_x'] = gdf_clipped['centroid'].apply(lambda x: x.x)
+gdf_clipped['centroid_y'] = gdf_clipped['centroid'].apply(lambda x: x.y)
+gdf_clipped[['id', 'centroid_x', 'centroid_y']].to_csv('existing_solar_farms.csv')
 
 ax = az.plot()
 gdf_clipped['centroid'].plot(ax=ax, color='red', markersize=5)
