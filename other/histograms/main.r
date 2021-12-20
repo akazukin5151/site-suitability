c8010cd8a4728c50cb6956a6b177db9cb7f8ec6d
twenty5151 <tsuiyikching@protonmail.com>
2021-12-20 14:20:06 +0000

    Move histogram script to the `other` dir

--- other/histograms/main.r
+++ other/histograms/main.r
@@ -2,25 +2,25 @@ library(raster)
 library(rgdal)
 pdf(NULL)
 
-# These images are from the out dir (not public; adapt path)
-a <- '../asakareh_final.tif'
-w <- '../watson_final.tif'
-s <- '../../../suh/final_clipped.tif'
+# These images are from the out dir (not public; adapt path if needed)
+a <- '../../out/asakareh/final_clipped.tif'
+w <- '../../out/watson/final_clipped.tif'
+s <- '../../out/suh/final_clipped.tif'
 
 r <- raster(x = s)
 #plot(r)
 rs <- sample(r, size=10000)
-png("suh_hist.png")
+png("out/suh_hist.png")
 hist(rs[rs != 0], xlab="Suitability score", main="Histogram of suitability scores (Suh)")
 
 r <- raster(x = a)
 rs <- sample(r, size=10000)
-png("a_hist.png")
+png("out/a_hist.png")
 hist(rs[rs != 0], xlab="Suitability score", main="Histogram of suitability scores (Asakereh)")
 
 r <- raster(x = w)
 rs <- sample(r, size=10000)
-png("w_hist.png")
+png("out/w_hist.png")
 hist(rs[rs != 0], xlab="Suitability score", main="Histogram of suitability scores (Watson)")
 
 dev.off()
