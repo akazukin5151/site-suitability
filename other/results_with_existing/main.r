b8a84927ba3de5de99e4c5a67bd2d6226518269e
twenty5151 <tsuiyikching@protonmail.com>
2021-12-20 14:20:49 +0000

    Move results with existing plot to the `other` dir

--- other/results_with_existing/main.r
+++ other/results_with_existing/main.r
@@ -4,28 +4,28 @@ library(raster)
 pdf(NULL)
 
 # These images are from the out dir (not public; adapt path)
-a <- '../asakareh_final.tif'
-w <- '../watson_final.tif'
-s <- '../../../suh/final_clipped.tif'
+a <- '../../out/asakareh/final_clipped.tif'
+w <- '../../out/watson/final_clipped.tif'
+s <- '../../out/suh/final_clipped.tif'
 existing <- read.csv(
-    '../../../../other/existing_solar_farms/existing_solar_farms.csv'
+    '../existing_solar_farms/existing_solar_farms.csv'
 )
 x = existing[, 'centroid_x']
 y = existing[, 'centroid_y']
 
 
 r <- raster(x = s)
-png("suh.png")
+png("out/suh.png")
 plot(r, main="Suh")
 points(x=x, y=y, pch='.', col='red', cex=5)
 
 r <- raster(x = a)
-png("asakereh.png")
+png("out/asakereh.png")
 plot(r, main="Asakareh")
 points(x=x, y=y, pch='.', col='red', cex=5)
 
 r <- raster(x = w)
-png("watson.png")
+png("out/watson.png")
 plot(r, main='Watson')
 points(x=x, y=y, pch='.', col='red', cex=5)
 
